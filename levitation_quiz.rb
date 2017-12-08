f6938774997c08c0e3d61c5e83d4233d5c0af458
apocalypse575 <apocalypse575@gmail.com>
2017-12-08 00:33:38 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,3 +2,6 @@
 def levitation_quiz
 
 end
+
+#levitation_quiz puts out the question 'What is the spell that enacts levitation?' until a user types 'Wing
+# ardium Leviosa'. Then, it puts out 'You passed the quiz!'
